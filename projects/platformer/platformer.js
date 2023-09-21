2589ae430da632c850c1c2a0ca95a29a983ebb56
cameron ____ <flameguy0104@gmail.com>
2023-09-21 22:00:54 +0000

    add index.html and css files

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -28,12 +28,12 @@ $(function () {
      * Comment the lines out to remove the grid
      */
 
-    // for (let i = 100; i < canvas.width; i += 100) {
-    //   createPlatform(i, canvas.height, -1, -canvas.height);
-    // }
-    // for (let i = 100; i < canvas.height; i += 100) {
-    //   createPlatform(canvas.width, i, -canvas.width, -1);
-    // }
+    for (let i = 100; i < canvas.width; i += 100) {
+      createPlatform(i, canvas.height, -1, -canvas.height);
+    }
+    for (let i = 100; i < canvas.height; i += 100) {
+      createPlatform(canvas.width, i, -canvas.width, -1);
+    }
 
     /////////////////////////////////////////////////
     //////////ONLY CHANGE BELOW THIS POINT///////////
@@ -43,6 +43,14 @@ $(function () {
     // Create platforms
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
+    createPlatform(1200, 620, 200, 60)
+    createPlatform(900, 500, 200, 60)
+    createPlatform(370, 500, 200, 60)
+    createPlatform(300, 400, 200, 60)
+    createPlatform(500, 300, 30, 20)
+    createPlatform(600, 200, 200, 60)
+    createPlatform(1000, 250, 200, 60)
+    createPlatform(1200, 140, 100, 20)
 
 
 
