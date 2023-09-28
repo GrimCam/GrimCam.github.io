7a4e4b37df98b395c0d9169202e4ada9428c8203
cameron ____ <flameguy0104@gmail.com>
2023-09-28 22:06:41 +0000

    forgot to push up a few days ago

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -59,16 +59,25 @@ $(function () {
     // Create collectables
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
-    // example usage: createCollectable(type, x, y, gravity, bounce)
-
-
-
-
+    // example usage: createCollectable(type, x, y, gravity, bounce)   
+    createCollectable("database", 500, 300, 20, 0.5)   
+    createCollectable("diamond", 100, 700, 20, 0.5)
+    createCollectable("steve", 300, 300, 20, 0.5)
+    createCollectable("database", 700, 150, 20, 0.5)
+    createCollectable("database", 1000, 125, 20, 0.5) 
+    createCollectable("database", 900, 400, 20, 0.5)
+    createCollectable("database", 1250, 75, 20, 0.5)
     // TODO 3
     // Create cannons
     // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
+    createCannon("left", 400, 2000)
+    createCannon("bottom", 400, 1000)
+    createCannon("bottom", 900, 1000)
+    createCannon("left", 100, 1250)
+    
+
 
 
 
